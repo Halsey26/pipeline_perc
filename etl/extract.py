1e86203ea77a64cc358375ff87f4f7494aac0eec
Halsey26 <omara.cassano@gmail.com>
2025-08-26 22:35:38 -0500

    extract complete

--- etl/extract.py
+++ etl/extract.py
@@ -4,15 +4,41 @@ import pandas as pd
 import requests
 from requests.auth import HTTPBasicAuth
 from tqdm import tqdm
-
+from sqlalchemy import create_engine
+import json
+import numpy as np
+from pathlib import Path
 
 load_dotenv()
 
 login =  os.getenv('JUMPSELLER_LOGIN')
 authtoken =  os.getenv('JUMPSELLER_AUTHTOKEN')
 
+# FUNCIONES PARA EL PROCESO DE EXTRACCIÓN
+# Objectivo: extraer los datos raw de la api de jumpseller para enviarlos a supabase
+# JUMPSELLER -> SUPABASE
+
+def conect2supabase():
+    # Fetch variables
+    USER = os.getenv("user")
+    PASSWORD = os.getenv("password")
+    HOST = os.getenv("host")
+    PORT = os.getenv("port")
+    DBNAME = os.getenv("dbname")
+
+    # Construct the SQLAlchemy connection string
+    DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+    # Create the SQLAlchemy engine
+    engine = create_engine(DATABASE_URL)
+    print('✅ Supabase: Conexión Exitosa')
+    return engine
 
 def obtener_datos(endpoint, login, authtoken):
+    # Asegurar que la carpeta exista
+    Path("data/raw").mkdir(parents=True, exist_ok=True)
+
+
     datos = []
     page = 1
     barra= tqdm(desc=f"Descargando '{endpoint}'", unit=" páginas")
@@ -35,11 +61,51 @@ def obtener_datos(endpoint, login, authtoken):
     barra.close()
 
 
-    print(f"✅ Data obtenida: {endpoint.title()}. Total páginas {page-1}")
+    print(f"✅ Data obtenida: {endpoint.title()}. Total páginas {page-1}\n")
     datos= pd.json_normalize(datos)
-    datos.to_parquet(f"data/raw/{endpoint}_raw.parquet", index=False)
     return datos
 
+def to_serializable(obj):
+    if isinstance(obj, np.ndarray):
+        return [to_serializable(x) for x in obj.tolist()]
+    elif isinstance(obj, dict):
+        return {k: to_serializable(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [to_serializable(x) for x in obj]
+    else:
+        return obj  # valores simples
+
+def preprocessing_supabase(data_extracted):
+    
+    col_to_serializar=[]
+    for col in data_extracted.columns:
+        if data_extracted[col].apply(lambda x: isinstance(x, (np.ndarray, dict, list))).any(): 
+            col_to_serializar.append(col)
+    # ['product.categories', 'product.images', 'product.variants', 'product.fields]
+
+    # print(col_to_serializar)
+    if col_to_serializar: # si hay columnas 
+        for col in col_to_serializar:
+            # data_extracted[col]=data_extracted[col].apply(lambda fila: json.dumps(fila.tolist()) if isinstance(fila, np.ndarray) else json.dumps(fila))
+
+            data_extracted[col] = data_extracted[col].apply( lambda fila: json.dumps(to_serializable(fila)) )
+            
+        print(f'Dataframe serializado. \nColumnas Serializadas: {col_to_serializar}\n')
+    else:
+        print('Nada que serializar')
 
 
+def insert2supabase(engine,esquema, endpoint, df):
+    try: 
+        table_name= f'{endpoint}_raw'
+        df.to_sql(
+            table_name,
+            con=engine,
+            schema=esquema, # el esquema debe estar creado previamente
+            if_exists='append', #append para no soreescrbbir, ()
+            index=False
+        )
+        print(f'💾 Creación exitosa de la tabla {table_name} en el esquema {esquema}.\n')
+    except Exception as e:
+        print(f"Error para crear la tabla {table_name}: {e}")
 
--- prefect_flows/main.py
+++ prefect_flows/main.py
@@ -1,31 +1,56 @@
 import os
 from dotenv import load_dotenv
 from prefect import flow, task
-from etl.extract import obtener_datos
+from pipeline_perc.etl.extract import obtener_datos, conect2supabase,preprocessing_supabase,insert2supabase
 from pathlib import Path
-
+from sqlalchemy import create_engine
+from tqdm import tqdm
+# para ejecutar en terminal de venv: python -m pipeline_perc.prefect_flows.main
 
 login =  os.getenv('JUMPSELLER_LOGIN')
 authtoken =  os.getenv('JUMPSELLER_AUTHTOKEN')
+
 
 @task
 def extract():
     # Crear carpeta data/raw si no existe
     Path("data/raw").mkdir(parents=True, exist_ok=True)
+    
+    engine_supabase=conect2supabase() # en local, en producción usar Blocks
+    with engine_supabase.connect() as connection:
+        endpoints=[ "products","customers","orders"]
 
-    df_products = obtener_datos("products", login, authtoken)
-    df_customers = obtener_datos("customers", login, authtoken)
-    df_orders = obtener_datos("orders", login, authtoken)
+        # Obtiene los datos
+        dataframes={}
+        for endpoint in endpoints:
+            dataframes[f'df_{endpoint}']= obtener_datos(endpoint, login, authtoken)
 
-    df_products.to_parquet("data/raw/products_raw.parquet", index=False)
-    df_customers.to_parquet("data/raw/customers_raw.parquet", index=False)
-    df_orders.to_parquet("data/raw/orders_raw.parquet", index=False)
+        # Preprocesa para serializar
+        for key, df in dataframes.items():
+            print(key)
+            preprocessing_supabase(df)
 
-    return df_products, df_customers, df_orders
+        # Inserción en Supabase de data raw
+        esquema= 'raw'
+        for key, df in tqdm(dataframes.items(), desc= 'Insertando en Supabase'):
+            for endpoint in endpoints:
+                if key.endswith(endpoint):
+                    insert2supabase(engine_supabase,esquema, endpoint, df)
+
+    #     df_products = obtener_datos("products", login, authtoken)
+    #     df_customers = obtener_datos("customers", login, authtoken)
+    #     df_orders = obtener_datos("orders", login, authtoken)
+
+    #     df_products.to_parquet("data/raw/products_raw.parquet", index=False)
+    #     df_customers.to_parquet("data/raw/customers_raw.parquet", index=False)
+    #     df_orders.to_parquet("data/raw/orders_raw.parquet", index=False)
+
+    # return df_products, df_customers, df_orders
 
 @flow
 def main_flow():
-    productos, customers, orders= extract()
+    extract()
+    # productos, customers, orders= extract()
 
 if __name__ == "__main__":
     main_flow()
